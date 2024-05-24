921b51ea855fb8055793fea69f89b96aacba1ba5
Cyrix <cyrix326@gmail.com>
2024-05-24 12:51:04 +1200

    qb-okok Notify fix

--- config.lua
+++ config.lua
@@ -59,40 +59,48 @@ Config.Lang = {
     ["too_fast"] = "Your doing this too fast. Please try again!"
 }
 
-function NotifyClientAlert(titletext, msgtext, type)
-    if Config.NotifyType == "qb" then
-        if type == 'inform' then
-            local info = 'primary'
-        QBCore.Functions.Notify(msgtext, info, 5000)
-        end
-    elseif Config.NotifyType == "ox" then
-        lib.notify({ title = titletext, description = msgtext, type = type })
-    elseif Config.NotifyType == "okok" then
-        if type == 'inform' then
-            local info = 'info'
-        exports['okokNotify']:Alert(titletext, msgtext, 5000, info)
-        end
-    elseif Config.NotifyType == "mythic" then
-        exports['mythic_notify']:DoHudText(type, msgtext, { ['background-color'] = '#ffffff', ['color'] = '#000000' })
-    elseif Config.NotifyType == "custom" then
+function NotifyClientAlert(titletext, msgtext, type, time)
+  if Config.NotifyType == "qb" then
+  if type == 'inform' then
+    local info = 'primary'
+       QBCore.Functions.Notify(msgtext, info, time)
+   else
+       QBCore.Functions.Notify(msgtext, type, time)
+    end
+  elseif Config.NotifyType == "ox" then
+       lib.notify({ title = titletext, description = msgtext, duration = time, type = type })
+  elseif Config.NotifyType == "okok" then
+  if type == 'inform' then
+    local info = 'info'
+       exports['okokNotify']:Alert(titletext, msgtext, time, info)
+   else
+       exports['okokNotify']:Alert(titletext, msgtext, time, type)
+    end
+  elseif Config.NotifyType == "mythic" then
+       exports['mythic_notify']:DoHudText(type, msgtext, { ['background-color'] = '#ffffff', ['color'] = '#000000' })
+  elseif Config.NotifyType == "custom" then
    end
 end
 
-function NotifyServerAlert(src, titletext, msgtext, type)
-    if Config.NotifyType == "qb" then
-        if type == 'inform' then
-           local info = 'primary'
-        TriggerClientEvent('QBCore:Notify', src, msgtext, info)
-        end
-    elseif Config.NotifyType == "ox" then
-        TriggerClientEvent('ox_lib:notify', src, { title = titletext, description = msgtext, type = type })
-    elseif Config.NotifyType == "okok" then
-        if type == 'inform' then
-            local info = 'info'
-        TriggerClientEvent('okokNotify:Alert', src, titletext, msgtext, 5000, info)
-        end
-    elseif Config.NotifyType == "mythic" then
+function NotifyServerAlert(src, titletext, msgtext, type, time)
+  if Config.NotifyType == "qb" then
+  if type == 'inform' then
+    local info = 'primary'
+       TriggerClientEvent('QBCore:Notify', src, msgtext, info)
+   else
+       TriggerClientEvent('QBCore:Notify', src, msgtext, type)
+    end
+  elseif Config.NotifyType == "ox" then
+       TriggerClientEvent('ox_lib:notify', src, { title = titletext, description = msgtext, duration = time,  type = type })
+  elseif Config.NotifyType == "okok" then
+  if type == 'inform' then
+    local info = 'info'
+       TriggerClientEvent('okokNotify:Alert', src, titletext, msgtext, time, info)
+   else
+       TriggerClientEvent('okokNotify:Alert', src, titletext, msgtext, time, type)
+    end
+  elseif Config.NotifyType == "mythic" then
         TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = type, text = msgtext, style = { ['background-color'] = '#ffffff', ['color'] = '#000000' } })
-    elseif Config.NotifyType == "custom" then
+  elseif Config.NotifyType == "custom" then
    end
 end
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 author 'Cyrix'
 description 'C-Breathalyzer'
-version '1.1'
+version '1.1.1'
 
 client_scripts {
     'client/*.lua'
